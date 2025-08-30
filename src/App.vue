63b824b828aad026ecd7e723c697bdf3207182ea
Jordi Hoven <hovenjordi@gmail.com>
2025-08-30 16:35:03 +0200

    add supabase

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabase.ts'
 
-<template>Tastebuddies</template>
+onMounted(async () => {
+  const { data, error } = await supabase.from('recipes').select('*')
+  if (error) console.error(error)
+  else recipes.value = data
+})
+
+interface Recipe {
+  id: string
+  name: string
+}
+const recipes = ref<Recipe[]>([])
+</script>
+
+<template>
+  <p>Tastebuddies</p>
+  <ul>
+    <li v-for="r in recipes" :key="r.id">{{ r.name }}</li>
+  </ul>
+</template>
 
 <style scoped></style>
